af85699713be79cc8ce102e5bf7518ba4dc4b763
David Razmadze <razd45@gmail.com>
2021-10-27 17:20:39 -0400

    Remove video

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -6,8 +6,6 @@ import "../../App.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/public/videos/video-2.mp4" autoPlay loop muted />
-
       <h1>Welcome to the club!</h1>
 
       <h3>Official Cybersecurity club @ University of Massachusetts, Lowell</h3>
