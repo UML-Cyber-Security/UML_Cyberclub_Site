3766a012a3500d04c85a827c991ee6525cf28aa7
David Razmadze <razd45@gmail.com>
2021-10-26 21:51:22 -0400

    Create NavBar and HeroSection

---
+++ src/components/HeroSection/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./style.css";
+import "../../App.css";
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <video src="/videos/video-2.mp4" autoPlay loop muted />
+
+      <h1>Welcome to the club!</h1>
+
+      <h3>Official Cybersecurity club @ University of Massachusetts, Lowell</h3>
+    </div>
+  );
+}
+
+export default HeroSection;
